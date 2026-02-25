588d9d3b5d238c1756453796448ba34adb00d1ac
Aya <ayahathout1@gmail.com>
2026-02-25 21:47:11 +0200

    feat: add JWTAuthenticationFilter

---
+++ src/main/java/com/ayahathout/spring_security_with_jwt/configurations/JWTAuthenticationFilter.java
@@ -0,0 +1,21 @@
+package com.ayahathout.spring_security_with_jwt.configurations;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
