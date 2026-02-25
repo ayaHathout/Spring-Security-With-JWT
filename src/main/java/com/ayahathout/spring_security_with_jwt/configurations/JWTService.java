4dfc67c34280a3db82dfdbfe225eb1562ffa109b
Aya <ayahathout1@gmail.com>
2026-02-26 01:08:29 +0200

    feat: add generateToken methods in JWTService

--- src/main/java/com/ayahathout/spring_security_with_jwt/configurations/JWTService.java
+++ src/main/java/com/ayahathout/spring_security_with_jwt/configurations/JWTService.java
@@ -2,11 +2,19 @@ package com.ayahathout.spring_security_with_jwt.configurations;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.format.annotation.DurationFormat;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -14,10 +22,14 @@ public class JWTService {
     @Value("${jwt.secret.key}")
     private String SECRET_KEY;
 
+    public Key getSecretKey() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(SECRET_KEY));
+    }
+
     // Extract all claims (payload) from the JWT
     public Claims getClaimsFromToken(String token) {
         return Jwts.parser()
-                .setSigningKey(Keys.hmacShaKeyFor(Decoders.BASE64.decode(SECRET_KEY)))
+                .setSigningKey(getSecretKey())
                 .build()
                 .parseSignedClaims(token)
                 .getBody();
@@ -32,5 +44,22 @@ public class JWTService {
     // Extract the username, sub from the JWT
     public String getUsernameFromToken(String token) {
         return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    // Generate JWT token with extra claims
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts
+                .builder()
+                .setExpiration(Date.from(Instant.now().plus(2, DurationFormat.Unit.DAYS.asChronoUnit())))
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .signWith(getSecretKey())
+                .compact();
+    }
+
+    // Generate JWT token without extra claims
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
     }
 }
